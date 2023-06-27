67cb1af1aab7a238e294d9072128f2d63ceacf00
Giuliano Geraci <giulianogeraci@gmail.com>
2023-06-27 22:08:18 +0200

    style: remove unused import

--- src/app/team/create/page.js
+++ src/app/team/create/page.js
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from 'react'
 import Form from 'components/molecules/Form/Form'
-import { useTrainer } from 'src/context/TrainerContext'
 
 export default function TeamCreate() {
   return (
