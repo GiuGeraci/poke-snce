6e625eb5caef133a337d0a4b4e436a0ceccfaa27
Giuliano Geraci <giulianogeraci@gmail.com>
2023-06-27 22:20:02 +0200

    fix create team and form

--- src/app/team/create/page.js
+++ src/app/team/create/page.js
@@ -1,11 +1,35 @@
 'use client'
-import { useState } from 'react'
 import Form from 'components/molecules/Form/Form'
+import { fetchApi } from 'helpers/fetchApi'
+import { useRouter } from 'next/navigation'
+import { useContext, useState } from 'react'
+import { TrainerContext } from 'src/context/TrainerContext'
 
 export default function TeamCreate() {
+  const { trainer } = useContext(TrainerContext)
+  const [name, setName] = useState('')
+  const router = useRouter()
+
+  const onInput = (event) => setName(event.target.value)
+  async function createTeam(event) {
+    event.preventDefault()
+    if (name && name != '') {
+      const { data, statusCode } = await fetchApi({
+        path: 'teams',
+        method: 'POST',
+        body: {
+          name,
+          trainer_id: trainer.id,
+        },
+      })
+      if (statusCode === 200 && data?.createdTeam) {
+        router.push(`/team/${data?.createdTeam.id}/edit`)
+      }
+    }
+  }
   return (
     <div>
-      <Form label={'Create'}></Form>
+      <Form label={'Create'} onSubmit={createTeam} onInput={onInput}></Form>
     </div>
   )
 }
--- src/components/molecules/Form/Form.js
+++ src/components/molecules/Form/Form.js
@@ -1,51 +1,21 @@
 import PropTypes from 'prop-types'
 import styles from './Form.module.css'
 import Button from 'components/atoms/Button/Button'
-import { useContext, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { TrainerContext } from 'src/context/TrainerContext'
 
-export default function Form({ label }) {
-  const { trainer } = useContext(TrainerContext)
-  const [name, setName] = useState('')
-  const router = useRouter()
-
-  async function createTeam(event) {
-    event.preventDefault()
-    if (name && name != '') {
-      const res = await fetch(`http://localhost:3000/api/teams`, {
-        method: 'POST',
-        body: JSON.stringify({
-          name,
-          trainer_id: trainer?.id,
-        }),
-      })
-      try {
-        const { createdTeam } = await res.json()
-        router.push(`/teams/${createdTeam.id}/edit`)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    console.log(name)
-  }
+export default function Form({ label, onSubmit, onInput }) {
   return (
-    <form onSubmit={createTeam}>
+    <form onSubmit={onSubmit}>
       <div>
         <h1>{label}</h1>
       </div>
-      <input
-        name="name"
-        onInput={(event) => setName(event.target.value)}
-        placeholder="Team name"
-      />
+      <input name="name" onInput={onInput} placeholder="Team name" />
       <input hidden defaultValue={1} name="trainer_id" />
       <Button label={label} type="submit"></Button>
     </form>
   )
 }
 
-Button.propTypes = {
+Form.propTypes = {
   label: PropTypes.string,
   onSubmit: PropTypes.func,
   trainer_id: PropTypes.number,
