26afe843a14815c84a2370f23fc7c9026560b4c0
Giuliano Geraci <giulianogeraci@gmail.com>
2023-06-27 20:45:17 +0200

    style&doc: changed fetchApi

--- src/helpers/fetchApi.js
+++ src/helpers/fetchApi.js
@@ -1,18 +1,27 @@
-export const fetchApi = async ({ path, body, method = 'GET' }) => {
-    const options = {
-        method
-    }
+/**
 
-    if (body !== undefined) {
-        options.body = JSON.stringify(body)
-    }
+    Performs an asynchronous API request.
+    @param {string} params.path - The path of the API endpoint.
+    @param {Object} [params.body] - The request body (optional).
+    @param {string} [params.method='GET'] - The HTTP method (default: 'GET').
+    @property {Object} data - The parsed response data.
+    @property {number} statusCode - The HTTP status code of the response.
+    */
+export async function fetchApi({ path, body, method = 'GET' }) {
+  const options = {
+    method,
+  }
 
-    const response = await fetch(`http://localhost:3000/api/${path}`, options)
+  if (body !== undefined) {
+    options.body = JSON.stringify(body)
+  }
+  console.log(process.env.SERVER_HOST)
+  const response = await fetch(`${process.env.SERVER_HOST}/${path}`, options)
 
-    const parsedResponse = await response.json()
+  const parsedResponse = await response.json()
 
-    return {
-        data: parsedResponse,
-        statusCode: response.status
-    }
-  } 
+  return {
+    data: parsedResponse,
+    statusCode: response.status,
+  }
+}
