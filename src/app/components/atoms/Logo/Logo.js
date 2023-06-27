4ebb3436baa94e734ec651aefc724463ba4f2851
Giuliano Geraci <giulianogeraci@gmail.com>
2023-06-27 13:05:07 +0200

    fix: add logo atom

---
+++ src/app/components/atoms/Logo/Logo.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+import styles from './logo.module.css'
+import Image from 'next/image'
+
+export default function Logo({ onClick }) {
+  return (
+    <div className={styles.logo} onClick={onClick}>
+      <Image
+        src="/Pokemon-Logo-Background-PNG.png"
+        alt="Pokemon Logo"
+        width={200}
+        height={200}
+      />
+    </div>
+  )
+}
+
+Logo.propTypes = {
+  onClick: PropTypes.func,
+}
