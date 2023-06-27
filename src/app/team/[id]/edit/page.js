cacde0bf78887c7c3977a4a587fd0871721307b0
Giuliano Geraci <giulianogeraci@gmail.com>
2023-06-27 23:46:41 +0200

    feat: add form in edit, added check if user doesn't exist

--- src/app/team/[id]/edit/page.js
+++ src/app/team/[id]/edit/page.js
@@ -4,11 +4,14 @@ import ScrollableList from 'components/ScrollableList/ScrollableList'
 import Pokemon from 'components/Pokemon/PokemonInfo/PokemonCard'
 import { useEffect, useState } from 'react'
 import { fetchApi } from 'helpers/fetchApi'
+import Form from 'components/molecules/Form/Form'
+import { useRouter } from 'next/navigation'
 
 export default function TeamCreate({ params }) {
   const [pokemonList, setPokemonList] = useState([])
-
-  async function create() {
+  const [team, setTeam] = useState([])
+  const router = useRouter()
+  async function addPokemon() {
     const { data, statusCode } = await fetchApi({
       path: `teams/${params.id}/pokemon`,
       method: 'POST',
@@ -16,6 +19,25 @@ export default function TeamCreate({ params }) {
 
     if (statusCode === 200)
       setPokemonList((oldValue) => [...oldValue, data?.pokemon])
+  }
+
+  const onInput = (event) => setName(event.target.value)
+
+  async function editTeam(event) {
+    event.preventDefault()
+    if (name && name != '') {
+      const { data, statusCode } = await fetchApi({
+        path: 'teams',
+        method: 'PATCH',
+        body: {
+          name,
+        },
+      })
+
+      if (statusCode === 200) {
+        setTeam(data?.team)
+      }
+    }
   }
 
   useEffect(() => {
@@ -26,26 +48,42 @@ export default function TeamCreate({ params }) {
 
       if (statusCode === 200) {
         setPokemonList(data?.team?.pokemon)
+        setTeam(data?.team)
+      } else {
+        router.push(`/`)
       }
     }
 
     retrieveTeam()
   }, [params.id])
 
+  console.log(team)
+
   return (
     <div>
-      <div>Modifica team</div>
-      <Button
-        label="Create pokemon"
-        onClick={create}
-        disabled={pokemonList.length >= 6}
-      ></Button>
       <div>
-        <ScrollableList>
-          {pokemonList.map((pokemon, index) => (
-            <Pokemon key={index} {...pokemon}></Pokemon>
-          ))}
-        </ScrollableList>
+        <div>Edit Team</div>
+        <Form
+          label={'Edit'}
+          onSubmit={editTeam}
+          onInput={onInput}
+          placeholder={team.name}
+        ></Form>
+      </div>
+      <div>
+        <div>Add Pokemon</div>
+        <Button
+          label="Get a random Pokemon"
+          onClick={addPokemon}
+          disabled={pokemonList.length >= 6}
+        ></Button>
+        <div>
+          <ScrollableList>
+            {pokemonList.map((pokemon, index) => (
+              <Pokemon key={index} {...pokemon}></Pokemon>
+            ))}
+          </ScrollableList>
+        </div>
       </div>
     </div>
   )
--- src/components/molecules/Form/Form.js
+++ src/components/molecules/Form/Form.js
@@ -2,14 +2,17 @@ import PropTypes from 'prop-types'
 import styles from './Form.module.css'
 import Button from 'components/atoms/Button/Button'
 
-export default function Form({ label, onSubmit, onInput }) {
+export default function Form({ label, onSubmit, onInput, placeholder = null }) {
   return (
     <form onSubmit={onSubmit}>
       <div>
         <h1>{label}</h1>
       </div>
-      <input name="name" onInput={onInput} placeholder="Team name" />
-      <input hidden defaultValue={1} name="trainer_id" />
+      <input
+        name="name"
+        onInput={onInput}
+        placeholder={placeholder || 'Team Name'}
+      />
       <Button label={label} type="submit"></Button>
     </form>
   )
@@ -19,4 +22,5 @@ Form.propTypes = {
   label: PropTypes.string,
   onSubmit: PropTypes.func,
   trainer_id: PropTypes.number,
+  placeholder: PropTypes.string,
 }
