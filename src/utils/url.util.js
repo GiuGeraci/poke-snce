c3e1a29127e7902a4284a9714aa2e44811d64b1f
Giuliano Geraci <giulianogeraci@gmail.com>
2023-06-27 22:08:35 +0200

    feat: add buildSearchParams method

--- src/utils/url.util.js
+++ src/utils/url.util.js
@@ -18,3 +18,27 @@ export function retrieveQueryParams({ req }) {
   })
   return queryParams
 }
+
+/**
+ * Builds URLSearchParams object based on the selected abilities, types, and trainer ID.
+ *
+ * @param {Array<string>} selectedAbilities - The selected abilities.
+ * @param {Array<string>} selectedTypes - The selected types.
+ * @param {string} trainerId - The trainer ID.
+ * @returns {URLSearchParams} - The URLSearchParams object containing the built parameters.
+ */
+export function buildSearchParams(selectedAbilities, selectedTypes, trainerId) {
+  let params = new URLSearchParams()
+
+  selectedAbilities.forEach((id) => {
+    params.append('abilities', id)
+  })
+
+  selectedTypes.forEach((id) => {
+    params.append('types', id)
+  })
+
+  params.append('trainer_id', trainerId)
+
+  return params.toString()
+}
