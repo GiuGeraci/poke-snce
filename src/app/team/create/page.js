31443ba7b748e38706ccf8c6a19a073e681e3352
Giuliano Geraci <giulianogeraci@gmail.com>
2023-06-27 20:28:26 +0200

    feat: create v1.1

--- src/app/team/create/page.js
+++ src/app/team/create/page.js
@@ -1,9 +1,6 @@
 'use client'
-import Button from 'app/components/atoms/Button/Button'
-import ScrollableList from 'app/components/ScrollableList/ScrollableList'
-import Pokemon from 'app/components/Pokemon/PokemonInfo/PokemonCard'
 import { useState } from 'react'
-import Form from 'app/components/molecules/Form/Form'
+import Form from 'components/molecules/Form/Form'
 import { useTrainer } from 'src/context/TrainerContext'
 
 export default function TeamCreate() {
